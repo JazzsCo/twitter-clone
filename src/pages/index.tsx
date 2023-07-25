56f6c0ac08eb2f22a7c25f60fbab08ba6e1c369f
Jzz-C <ygyi8632@gmail.com>
2023-07-25 21:23:49 +0630

    feat: added resuable modal component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import Modal from "@/components/Modal";
 
 export default function Home() {
   return (
     <>
       <Header showBackArrow label="Home" />
+      <Modal />
     </>
   );
 }
